0d024979f28fbd2bbc3944dbbc5c11dff5a45713
acmexii <apexacme@gmail.com>
2024-02-13 05:08:19 +0000

    customized

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install net-tools
 sudo apt install iputils-ping
 pip install httpie
-sdk install java 17.0.4.1-tem < /dev/null && 
+sdk install java 17.0.4.1-tem < /dev/null
 
 cd infra
 docker-compose up
